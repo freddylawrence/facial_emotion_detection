3b84029cb02ed49bcf90c72f570b71a2ff5bdfee
freddylawrence <freddylawrence6@gmail.com>
2023-09-05 16:24:09 +0530

    emotion model

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Facial_Emotion_Detection
+
+Number of images in data set:
+	
+	angry:  3995
+	disgusted: 436
+	fearful:4097
+	happy: 7215
+	neutral: 4965
+	sad: 4830
+	surprised: 3171
+
+Image size: 48*48
---
+++ face2.py
@@ -0,0 +1,53 @@
+import os
+import cv2
+import numpy as np
+from keras.preprocessing import image
+import warnings
+warnings.filterwarnings("ignore")
+from keras.preprocessing.image import load_img, img_to_array 
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+model = load_model("emotionmobilenet2.h5")
+
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+video_path="C:\\Users\\fredd\\Downloads\\newWhatsApp Video 2023-09-05 at 11.47.14.mp4"
+cap = cv2.VideoCapture(video_path)
+
+#cap = cv2.VideoCapture(0)
+
+while True:
+    ret, test_img = cap.read()  
+    if not ret:
+        break
+    
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+    for (x, y, w, h) in faces_detected:
+        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+        roi_gray = gray_img[y:y + h, x:x + w] 
+        roi_gray = cv2.resize(roi_gray, (224, 224))
+        img_pixels = image.img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255
+
+        predictions = model.predict(img_pixels)
+        max_index = np.argmax(predictions[0])
+
+        emotions = ('angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised')
+        predicted_emotion = emotions[max_index]
+
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    resized_img = cv2.resize(test_img, (1000, 700))
+    cv2.imshow('Facial emotion analysis', resized_img)
+
+    if cv2.waitKey(10) == ord('q'): 
+        break
+
+cap.release()
+cv2.destroyAllWindows()
